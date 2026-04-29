d653be56c9b3b08902007f058f858871d1b07461
Captain Harris <67209324+cptharris@users.noreply.github.com>
2026-04-29 10:03:53 -0700

    changes to architecture idea

--- HW2/backend/agent.py
+++ HW2/backend/agent.py
@@ -15,16 +15,17 @@ a query arrives, containing the PROMPT and the DATESET to evaluate on
 
 # DESCRIPTION OF NODES
 
-[history] (calls LLM)
+[history]
 - adds recent_context (Q/A pairs)
 - adds previous_data and previous_data_description
-- given recent_context and previous_data_description, is this a follow-up prompt (boolean)?
 
-[describe_dataset] (conditional node, not follow-up)
+[describe_dataset]
 - adds dataset_description
     - this helps LLM calls understand the available dataset
 
 [planner] (calls LLM)
+- given recent_context and previous_data_description, is this a follow-up prompt (boolean)?
+    - a follow-up is not allowed to access the original dataset again
 - should we generate a text output (boolean)?
     - this is almost always true, unless the prompt is specifically asking for a visualization
     - clearly and concisely state the QUESTION that the text output must answer (considering recent_context if this is a follow-up question)
@@ -34,8 +35,9 @@ a query arrives, containing the PROMPT and the DATESET to evaluate on
     - make sure to be broad in the captured information so we can generate a visualization (if necessary) and answer follow-up  questions
 
 [codegen] (calls LLM)
-- given a dataframe called `df`
-- must create a variable called `data`, which should be a dict or dataframe (not print statements)
+- given a dataframe called `df` and its description
+    - if this is a follow-up, the dataframe will be the previous run's data
+- must create a variable called `data`, which should be a JSON-safe dict (not print statements)
 - do NOT include explanations; output ONLY Python code
 - generate code that captures necessary DATA
 
@@ -43,13 +45,14 @@ a query arrives, containing the PROMPT and the DATESET to evaluate on
 - execute the Python code created by codegen
 - capture the data
 - handle errors
-  - the agent should stop work and return errors here, no sense in continuing with no data
+    - retry from codegen once
+    - otherwise, stop work and return errors here, no sense in continuing with no data
 
 [describe_data]
 - adds data_description
     - this helps LLM calls understand the captured data
 
-[visualize] (calls LLM) (conditional node)
+[visualize-codegen] (calls LLM) (conditional node)
 - given the captured data and the visualization goal, generate code for the visualization
     - result is preloaded
     - use plotly.express as px
@@ -57,7 +60,13 @@ a query arrives, containing the PROMPT and the DATESET to evaluate on
     - avoid non-JSON serializable objects
     - do not call fig.show()
     - output ONLY Python code
-- execute the code, capture the figure plotly json
+
+[visualize-execute]
+- execute the code created by visualize-codegen
+- capture the figure plotly json
+- handle errors
+    - retry from visualize-codegen
+    - otherwise, give up on visualization
 
 [respond] (calls LLM) (conditional node)
 - given the captured data and the question, answer the question
@@ -72,6 +81,44 @@ a query arrives, containing the PROMPT and the DATESET to evaluate on
 
 generally, we want to keep enough information from the dataset to answer the question, generate the visualization, and answer potential follow-up questions
 however, we also want to avoid passing a lot of data to the LLM (this will be a problem for the respond node, which must synthesize the data into a text response to the question)
+
+# STATE DICT
+{
+    "metadata": {
+        "session_id": str,
+        "run_id": str,
+        "prompt": str,
+        "dataset": str,
+        "step": str,
+    },
+    "recent_context": str,
+    "previous": {
+        "data": {},
+        "data_description": {
+            "column_types": {},
+        }
+    },
+    "plan": {
+        "is_follow_up": bool,
+        "do_response": bool,
+        "do-visualization": bool,
+        "question": str,
+        "visualization_goal": str,
+        "need_data": str,
+    },
+    "information": {
+        "data_code": str,
+        "data": {},
+        "data_description": {},
+        "error": str,
+    },
+    "visualization": {
+        "visualize_code": str,
+        "fig": {},
+        "error": str,
+    },
+    "response": str,
+}
 
 """
 
